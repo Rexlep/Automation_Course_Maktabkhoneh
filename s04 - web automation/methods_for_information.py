d62f471fa2ea3a42e8f1dd97a4542c4f487c1a21
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-01-26 12:44:59 +0330

    changes

--- s04 - web automation/methods_for_information.py
+++ s04 - web automation/methods_for_information.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://maktabkhooneh.org/")
 
-current_url = driver.current_url
-page_title = driver.title
-page_source = driver.page_source
-print(current_url)
-print(page_title)
-print(page_source)
+element = driver.find_element(By.XPATH, "/html/body/div[1]/section/main/section/div/div[1]/div[1]/div[1]/h4")
 
-driver.close()
-driver.quit()
+text = element.text
+attribute = element.get_attribute("class")
+css_value = element.value_of_css_property("font-size")
+location = element.location
+size = element.size
+
+print(text)
+print(attribute)
+print(css_value)
+print(location)
+print(size)
