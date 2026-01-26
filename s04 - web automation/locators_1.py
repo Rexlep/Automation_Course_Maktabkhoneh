cdb4c15344d7a73449ad499a68e78005a2a0fd2f
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-01-26 12:34:45 +0330

    changes

--- s04 - web automation/locators_1.py
+++ s04 - web automation/locators_1.py
@@ -1,12 +1,21 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://maktabkhooneh.org/")
 
+driver.refresh()
+current_url = driver.current_url
+page_title = driver.title
+page_source = driver.page_source
 
-input_site = driver.find_element(By.TAG_NAME, "input")
-input_site.send_keys("امیر مهدی گودرزی")
+# element = driver.find_element(By.TAG_NAME, "input")
+# element2 = driver.find_element(By.CLASS_NAME, "box")
+# element3 = driver.find_element(By.XPATH, "/html/body.//")
+# element4 = driver.find_element(By.CSS_SELECTOR, ".box")
+# element5 = driver.find_element(By.ID, "box")
+# element6 = driver.find_element(By.LINK_TEXT, "دوره های پایتون")
 
-input()
+input_by_class = driver.find_element(By.CLASS_NAME, "w-full")
+
+print(input_by_class)
