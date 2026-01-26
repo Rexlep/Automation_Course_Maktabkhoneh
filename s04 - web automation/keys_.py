7daa84f2f963f0a39c79b2deb9fc1848f01e7917
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-01-26 12:48:27 +0330

    changes

---
+++ s04 - web automation/keys_.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.keys import Keys
+
+element = None
+
+element.send_keys(Keys.ENTER)
+element.send_keys(Keys.SHIFT)
+element.send_keys(Keys.ESCAPE)
+element.send_keys(Keys.SPACE)
+element.send_keys(Keys.CONTROL + "a")
