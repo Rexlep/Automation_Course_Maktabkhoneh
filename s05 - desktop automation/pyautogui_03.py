98580cfc78a5f15f93dd363a116bf64f81674fbe
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-01-27 13:58:24 +0330

    Coded

---
+++ s05 - desktop automation/pyautogui_03.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+pyautogui.click()
+pyautogui.click(x=500, y=500)
+pyautogui.click(button="right")
+pyautogui.click(button="middle")
+
+pyautogui.doubleClick()
+pyautogui.rightClick()
+pyautogui.leftClick()
+pyautogui.tripleClick()
+pyautogui.middleClick()
