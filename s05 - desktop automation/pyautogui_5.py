57947522b96c3c106b775bf41cf0a70108883f86
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-01-27 14:25:32 +0330

    Coded

---
+++ s05 - desktop automation/pyautogui_5.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+# pyautogui.scroll(-1000)
+# pyautogui.scroll(-1000, x=100, y=0)
+# pyautogui.hscroll(-100)
+
+pyautogui.write("Hello World", interval=0.1)
+pyautogui.write("This is line1\n This is line2\n This is line3")
+pyautogui.write('special @#$%^&*()')
