e5364baad5517bdefaff377acc46e8a17f30fcc0
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-01-28 12:42:47 +0330

    Coded

--- s05 - desktop automation/pyautogui_08.py
+++ s05 - desktop automation/pyautogui_08.py
@@ -1,1 +1,11 @@
+import pyautogui
 
+project_part = pyautogui.locateOnScreen('windo.png')
+
+if project_part:
+    print('Founded')
+    center = pyautogui.center(project_part)
+    pyautogui.click(center)
+
+else:
+    print('Not Found')
