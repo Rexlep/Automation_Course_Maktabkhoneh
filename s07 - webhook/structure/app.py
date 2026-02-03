a01330b606894288688065090b52e54426f6240c
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-02-03 15:38:45 +0330

    coded

--- s07 - webhook/structure/app.py
+++ s07 - webhook/structure/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 
 
@@ -27,6 +29,15 @@ def update_status():
         return {'status': "updated"}, 200
 
     return {"error": "service not found"}, 404
+
+
+@app.route("/run-script", methods=['POST'])
+def update_service():
+    print("User clicked on button")
+
+    os.system("notepad.exe")
+
+    return '<h1>Success script is running now</h1><a href="/">Back to Home</a>'
 
 
 if __name__ == "__main__":
