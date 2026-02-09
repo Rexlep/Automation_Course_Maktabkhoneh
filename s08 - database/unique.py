1ed11b6d50cf06785a65f1351280ab82f5145f59
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-02-09 12:35:11 +0330

    Coded

---
+++ s08 - database/unique.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect("data.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+            CREATE TABLE IF NOT EXISTS members (
+                id INTEGER PRIMARY KEY,
+                username TEXT UNIQUE,
+                score INTEGER
+            )
+""")
+
+try:
+    cursor.execute("INSERT INTO members (username, score) VALUES (?, ?)",("Amirgd", "100"))
+    conn.commit()
+
+except sqlite3.IntegrityError:
+    print("This username already taken")
+
+conn.close()
