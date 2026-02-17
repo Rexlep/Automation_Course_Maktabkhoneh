c3399436f024cc40e4662ec5b1da1706cad2ad99
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-02-17 11:40:34 +0330

    Coded the traceback_call file with traceback content

---
+++ s11 - logging/traceback_call.py
@@ -0,0 +1,15 @@
+import logging
+
+logging.basicConfig(
+    filename="automation.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+try:
+    result = 10 / 4
+    logging.info(f"calculation done {result}", exc_info=True)
+
+except Exception as e:
+    logging.error("Calculation error", exc_info=True)
